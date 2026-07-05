f1ffeec856d40738d631ee85db62aabe0ba91cbb
Jeet Mondal <jm382118@gmail.com>
2026-07-05 11:20:56 +0530

    Files are Set Up  by JM

--- frontend/src/Landing_Page/Home/Hero.js
+++ frontend/src/Landing_Page/Home/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <h1>Hero <i class="fas fa-apple-alt"></i><i class="fa-brands fa-bluesky"></i></h1>
+        <h1>Hero <i class="fa-brands fa-bluesky"></i></h1>
         
      );
 }
